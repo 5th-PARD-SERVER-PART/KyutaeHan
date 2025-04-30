51d7c21b173e992353c9ad8a496e289512bf4048
rbxo8675 <rbxo8675@gmail.com>
2025-05-01 05:15:28 +0900

    .

---
+++ seminar4th/src/main/java/com/pard/server/seminar4th/Seminar4thApplication.java
@@ -0,0 +1,13 @@
+package com.pard.server.seminar4th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar4thApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Seminar4thApplication.class, args);
+    }
+
+}
---
+++ seminar4th/src/test/java/com/pard/server/seminar4th/Seminar4thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.server.seminar4th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar4thApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

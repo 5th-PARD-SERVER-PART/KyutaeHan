1e6528b8fcd134d4736f5b60244efd27e04f8f8e
rbxo8675 <rbxo8675@gmail.com>
2025-05-01 05:21:26 +0900

    delete seminar4

--- seminar4/src/main/java/com/pard/server/seminar4th/book/repo/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pard.server.seminar4th.book.repo;
-
-
-import com.pard.server.seminar4th.book.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Book, Long> {
-
-}

b3961d58231e591ee0bbe4108f56dba98573fbd4
rbxo8675 <rbxo8675@gmail.com>
2025-05-02 14:33:29 +0900

    init

---
+++ hw4_blog/src/main/java/com/pard/server/hw4_blog/Hw4BlogApplication.java
@@ -0,0 +1,13 @@
+package com.pard.server.hw4_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw4BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw4BlogApplication.class, args);
+    }
+
+}

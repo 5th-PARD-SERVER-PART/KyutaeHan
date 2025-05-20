d07b9bba0bc129dc100accc855f5e15d9ed820d2
rbxo <rbxo8675@gmail.com>
2025-05-20 22:30:15 +0900

    post

--- seminar6_re/src/main/java/com/pard/server/seminar6_re/user/controller/UserController.java
+++ seminar6_re/src/main/java/com/pard/server/seminar6_re/user/controller/UserController.java
@@ -1,12 +1,20 @@
 package com.pard.server.seminar6_re.user.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.pard.server.seminar6_re.user.dto.UserDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+
+    @PostMapping("")
+    public ResponseEntity<Void> createUser(@RequestBody UserDto.UserCreateRequest req){
+        userService.createUser(req);
+        return ResponseEntity.status(HttpStatus.CREATED).build;
+    }
 
 }
